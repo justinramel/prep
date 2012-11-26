0628cc6386eac879fef70251a5c23b1697e7420c
Jean-Paul S. Boodhoo <jp@developwithpassion.com>
2012-11-26 11:27:10 -0800

    Updated the setup code

--- build/support/002_configuration/git.rb
+++ build/support/002_configuration/git.rb
@@ -1,8 +1,8 @@
 configs ={
   :git => {
-    :user => 'camug2012nov',
-    :remotes => %w/mokhan iamfruin JamesKovacs gt-sdi epithanatios adamalinauskas luuduong andypaxo robdcng nakamura memon blitter da991319/,
-    :repo => 'code' 
+    :user => 'lasvegas2012',
+    :remotes => %w/mokhan justinramel/,
+    :repo => 'prep' 
   }
 }
 configatron.configure_from_hash(configs)
