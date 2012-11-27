c1fc840268faec9f455e0808b900caf348924eeb
Jean-Paul S. Boodhoo <jp@developwithpassion.com>
2012-11-27 11:59:39 -0800

    Updated

--- blocks.rb
+++ blocks.rb
@@ -8,7 +8,25 @@ end
 
 def multiply_by_prime(prime)
 end
+class SomeBigItem
+  
+end
 
-times_2 = multiply_by_prime(2)
-two_by_3 = times_2(3)
-p two_by_3.call
+def expensive_creation
+  lambda{
+  p 'I am creating it again'
+  SomeBigItem.new
+  }
+end
+
+class Proc
+  def yo
+    p 'yo'
+  end
+end
+
+
+factory = expensive_creation.memoize
+3.times do
+  factory.call
+end
