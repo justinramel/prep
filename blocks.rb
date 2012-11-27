8731b2d5ffac31262824af7b23896dfdb047e73a
Justin Ramel <justin.ramel@gmail.com>
2012-11-27 11:59:06 -0800

    Pushing new changes

--- blocks.rb
+++ blocks.rb
@@ -7,8 +7,9 @@ def multiply(first,second)
 end
 
 def multiply_by_prime(prime)
+  lambda{|number| multiply(prime, number)}
 end
 
 times_2 = multiply_by_prime(2)
-two_by_3 = times_2(3)
+two_by_3 = times_2.call(3)
 p two_by_3.call
