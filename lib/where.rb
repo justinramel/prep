211f35b886e345486b8d238405ef2f1971534736
Jean-Paul S. Boodhoo <jp@developwithpassion.com>
2012-11-27 17:32:03 -0800

    Updated

--- lib/where.rb
+++ lib/where.rb
@@ -21,12 +21,9 @@ class Where
   
   def create_matcher_using(match_strategy = NeverMatch.new,&block)
 
-    strategy = block_given? ? match_strategy : BlockMatch.new block
+    strategy = block_given? ? match_strategy : BlockMatch.new(block)
 
     SymbolicMatch.new(field,strategy)
-  end
-  def equal_to_any(*values)
-    create_matcher_using EqualToAnyMatch.new(*values)
   end
 
   def equal_to(value)
