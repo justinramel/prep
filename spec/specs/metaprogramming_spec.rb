14968c04324c9941add1f6eadfb0435aadda4d7c
Justin Ramel <justin.ramel@gmail.com>
2012-11-28 15:06:16 -0800

    Pushing new changes

--- spec/specs/metaprogramming_spec.rb
+++ spec/specs/metaprogramming_spec.rb
@@ -1,11 +1,12 @@
 #Metaprogramming Building Blocks
 #
 #define_method(name,&block)
+#instance_variable_{get,set} (@{name})
 
 class Object
   def self.accessors(*names)
     names.each do|attribute_name|
-
+     define_method(attribute_name, lambda { instance_variable_get("@#{attribute_name}") })
     end
   end
 end
@@ -22,7 +23,7 @@ describe 'Metaprogramming' do
   let(:age){34}
   let(:name){'JP'}
   let(:sut){Person.new(name,age)}
-  
+
   context 'adding attributes' do
     it 'should be able to add a smarter attribute assignment style' do
       sut.name.should == name
