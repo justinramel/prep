597dcb3900cf555ea37f698b310066a8377cdd84
Jean-Paul S. Boodhoo <jp@developwithpassion.com>
2012-11-28 15:01:43 -0800

    Updated

--- spec/specs/metaprogramming_spec.rb
+++ spec/specs/metaprogramming_spec.rb
@@ -1,7 +1,32 @@
+#Metaprogramming Building Blocks
+#
+#define_method(name,&block)
+
+class Object
+  def self.accessors(*names)
+    names.each do|attribute_name|
+
+    end
+  end
+end
+class Person
+  accessors :name,:age
+
+  def initialize(name,age)
+    @name = name
+    @age = age
+  end
+end
+
 describe 'Metaprogramming' do
+  let(:age){34}
+  let(:name){'JP'}
+  let(:sut){Person.new(name,age)}
+  
   context 'adding attributes' do
     it 'should be able to add a smarter attribute assignment style' do
-      
+      sut.name.should == name
+      sut.age.should == age
     end
   end
 end
